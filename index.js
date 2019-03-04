47a41d041da06e3ddfe9bbb919c9bd8301262b9c
Jm <jonathan.p.middleton@gmail.com>
2019-03-04 20:17:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ main.remove();
 /* Create your new element here and assign it to newHeader */
 const newHeader = document.createElement('h1')
 newHeader.id="victory";
-newHeader 
+newHeader.innerText = 'Jonathan is the champion!'
 
 
