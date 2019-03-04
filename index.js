bd673125cf71c81bf1062f2fba5cb866a77d62e5
Jm <jonathan.p.middleton@gmail.com>
2019-03-04 19:57:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,4 @@ main.remove()
 /* Create your new element here and assign it to newHeader */
 const newHeader = h1;
 var victory = document.createElement("H1");
-let h1 ="victory"
 H1="Jonathan is the champion!"
