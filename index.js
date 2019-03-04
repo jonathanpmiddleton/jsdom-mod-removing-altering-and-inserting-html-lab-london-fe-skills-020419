e80c1781fc062f4fd45ea1943f95ea78b68805f8
Jm <jonathan.p.middleton@gmail.com>
2019-03-04 20:10:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 main.remove();
 /* Create your new element here and assign it to newHeader */
 const newHeader = h1;
-var h1 = document.createH1('h1#victory');
+var h1 = document.createH1('Fuck this');
 
