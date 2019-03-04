e0fa8747974a07b54367105e0f7a2ee01c80024d
Jm <jonathan.p.middleton@gmail.com>
2019-03-04 19:54:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* Enter the code to remove the main node element under this comment */
 main.remove()
 /* Create your new element here and assign it to newHeader */
-const newHeader = H1;
+const newHeader = h1;
 var victory = document.createElement("H1");
+let h1 ="victory"
 H1="Jonathan is the champion!"
